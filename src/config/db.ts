5c5415e5d20c1995567878776bc77d07b5f34414
Carlos Cartín Cascante <88363806+CarlosCartinCascante@users.noreply.github.com>
2024-12-10 23:09:37 -0600

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 const db = new Sequelize('postgresql://rest_api_node_typescript_c5gd_user:nuC2YnHgKnvlhK5AcggicXadx4asADnF@dpg-ctaa3lggph6c73emjah0-a.oregon-postgres.render.com/rest_api_node_typescript_c5gd', {
     dialect: 'postgres',
+    dialectModule: pg,
     models: [__dirname + '/../models/**/*'],
     logging: false,
     dialectOptions: {
